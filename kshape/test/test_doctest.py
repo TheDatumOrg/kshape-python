d20334c2823f219fe717b12c0dcfc1a77d6022de
Jörg Thalheim <joerg@thalheim.io>
2017-09-21 11:20:23 +0100

    correctly discover doctests

--- kshape/test/test_doctest.py
+++ kshape/test/test_doctest.py
@@ -3,8 +3,6 @@ import unittest
 
 from kshape import core
 
-class DocTests(unittest.TestCase):
-  def test_core(self):
-      testSuite = unittest.TestSuite()
-      testSuite.addTest(doctest.DocTestSuite(core))
-      unittest.TextTestRunner(verbosity = 2).run(testSuite)
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(core))
+    return tests
